b3aadef1ad82b098b99ef223a6b7b277b1df083b
Sofia Ibañez <124284172+sofia-if@users.noreply.github.com>
2023-11-16 11:17:37 -0300

    Update javascript.js
    ultimo js

--- clase1-repasohtml/javascript.js
+++ clase1-repasohtml/javascript.js
@@ -1,6 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
     const tipoSeguroSelect = document.getElementById("tipoSeguro");
     const valorSeguroSpan = document.getElementById("valorSeguro");
+    const contactForm = document.getElementById("contactForm");
+    contactForm.addEventListener('submit',  (event) => {
+        event.preventDefault();
+        const formData = new FormData(contactForm);
+        const nombre = formData.get("nombre");
+        const apellido = formData.get("apellido");
+        const dni = formData.get("dni");
+        const email = formData.get("email");
+        const telefono = formData.get("telefono");
+        const tipoSeguro = formData.get("tipoSeguro");
+
+       const resumen= `Nombre: ${nombre}\nApellido ${apellido}\n DNI: ${dni}\n Email ${email}\n Telefono: ${telefono}\n Tipo de seguro: ${tipoSeguro}`;
+
+        Swal.fire({
+            title: "solicitud enviada",
+            text: resumen, 
+            icon: "success",
+            showConfirmButton: true,
+            allowEscapeKey: false,
+            allowOutsideClick: false,
+            confirmButtonText: "Cerrar",
+        }).then( result => {
+            if(result.isConfirmed){
+                contactForm.reset();
+            }
+        });
+    });
 
     tipoSeguroSelect.addEventListener("change", function(){
         const selectedOption = tipoSeguroSelect.value;
@@ -18,6 +45,6 @@ document.addEventListener('DOMContentLoaded', function(){
             default:
                 break;
         }
-        valorSeguroSpan.textContent = valorSeguro;
+        valorSeguroSpan.textContent = `$${valorSeguro}`;
     })
 });
